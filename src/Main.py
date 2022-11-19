c9eee7bc76c77ae0fc690884c84b40ca52544d8f
Rafael Marotta <rafaelmarottag@gmail.com>
2022-11-19 00:26:44 -0300

    Add gitignore

--- src/Main.py
+++ src/Main.py
@@ -1,26 +1,34 @@
 from MyGraph import MyGraph
-  
-## Have fun guys =) 
+
+# Have fun guys =)
 
 
 def example_not_directed_graph():
-    g = MyGraph(3) ## Create a new graph with 5 vertices
-    g.add_edge(0, 1) ## Create an edge from vertex 0 to vertex 1 
-    g.add_edge(0, 2) ## Create an edge from vertex 1 to vertex 2
+    g = MyGraph(3)  # Create a new graph with 5 vertices
+    g.add_edge(0, 1)  # Create an edge from vertex 0 to vertex 1
+    g.add_edge(0, 2)  # Create an edge from vertex 1 to vertex 2
     g.show()
-    
+
+
 def example_directed_graph():
     g = MyGraph(3, directed=True)
-    g.add_edge(0, 1) ## Create an edge from vertex 0 to vertex 1 
-    g.add_edge(0, 2) ## Create an edge from vertex 1 to vertex 2
+    g.add_edge(0, 1)  # Create an edge from vertex 0 to vertex 1
+    g.add_edge(0, 2)  # Create an edge from vertex 1 to vertex 2
     g.show()
-    
+
+
 def example_of_label():
     g = MyGraph(1)
-    g.set_vertex_custom_attr(0,"label","Hello World")
+    g.set_vertex_custom_attr(0, "label", "Hello World")
     print(g.get_vertex_custom_attr(0, "label"))
     g.show()
-    
-#example_not_directioned_graph()   
-example_directed_graph()
-#example_of_label()
+
+
+def example_full_graph():
+    g = MyGraph(10, directed=True, full=True)
+    g.show()
+
+example_full_graph()
+# example_not_directioned_graph()
+#example_directed_graph()
+# example_of_label()
