2c32f9c36b0443bc4e4f03c9a3f21da839a6a5f0
Rafael Marotta <rafaelmarottag@gmail.com>
2022-12-04 01:51:33 -0300

    Testes.py

---
+++ output.gml
@@ -0,0 +1,86 @@
+Creator "igraph version 0.10.2 Sun Dec  4 01:49:30 2022"
+Version 1
+graph
+[
+  directed 0
+  node
+  [
+    id 0
+    label 0
+    weight 0
+  ]
+  node
+  [
+    id 1
+    label 1
+    weight 1
+  ]
+  node
+  [
+    id 2
+    label 2
+    weight 2
+  ]
+  node
+  [
+    id 3
+    label 3
+    weight 3
+  ]
+  node
+  [
+    id 4
+    label 4
+    weight 4
+  ]
+  edge
+  [
+    source 1
+    target 0
+  ]
+  edge
+  [
+    source 2
+    target 0
+  ]
+  edge
+  [
+    source 3
+    target 0
+  ]
+  edge
+  [
+    source 4
+    target 0
+  ]
+  edge
+  [
+    source 2
+    target 1
+  ]
+  edge
+  [
+    source 3
+    target 1
+  ]
+  edge
+  [
+    source 4
+    target 1
+  ]
+  edge
+  [
+    source 3
+    target 2
+  ]
+  edge
+  [
+    source 4
+    target 2
+  ]
+  edge
+  [
+    source 4
+    target 3
+  ]
+]
--- src/MyGraph.py
+++ src/MyGraph.py
@@ -4,13 +4,15 @@ from igraph import Graph
 
 from bridge_utils import *
 
+
 class MyGraph:
     def __init__(self, g=None, n_vrt=None, n_edges=None, directed=False, full=False, random=False):
-        if g is not None: 
+        if g is not None:
             self.g = g
         elif random:
-            ## Generate a random graph with n_vrt vertices and n_edges edges with loop disabled and parallel
-            self.g = Graph.Erdos_Renyi(n=n_vrt, m=n_edges, directed=directed, loops=False)
+            # Generate a random graph with n_vrt vertices and n_edges edges with loop disabled and parallel
+            self.g = Graph.Erdos_Renyi(
+                n=n_vrt, m=n_edges, directed=directed, loops=False)
             self.export_graph()
         elif full:
             self.g = Graph.Full(n=n_vrt, directed=directed)
@@ -37,24 +39,24 @@ class MyGraph:
     def set_vertex_label(self, n_vertex, text):
         self.g.vs[n_vertex]["label"] = text
 
-    def get_vertex_value(self, val):
-        return self.g.vs[val]["weight"]
+    def get_vertex_value(self, n_vertex):
+        return self.g.vs[n_vertex]["weight"]
 
-    def set_vertx_value(self, val):
-        self.g.vs["weight"] = val
+    def set_vertx_value(self, n_vertex, val):
+        self.g.vs[n_vertex]["weight"] = val
 
-    def set_edge_label(self, text):
-        self.g.es["label"] = text
+    def set_edge_label(self, v1, v2, text):
+        self.g.es[self.g.get_eid(v1, v2)]["label"] = text
 
-    def set_edge_val(self, val):
-        self.g.es["weight"] = val
-        
-    def get_edge_label(self):
-        return self.g.es["label"]
-    
-    def get_edge_val(self):
-        return self.g.es["weight"]
-    
+    def set_edge_val(self,  v1, v2, val):
+        self.g.es[self.g.get_eid(v1, v2)]["weight"] = val
+
+    def get_edge_label(self,  v1, v2):
+        return self.g.es[self.g.get_eid(v1, v2)]["label"]
+
+    def get_edge_val(self,  v1, v2):
+        return self.g.es[self.g.get_eid(v1, v2)]["weight"]
+
     def vertex_with_value_exists(self, v):
         for i in range(self.g.vcount()):
             if self.g.vs[i]["weight"] == v:
@@ -79,7 +81,7 @@ class MyGraph:
         return False
 
     def is_edges_adjacents(self, v1, v2, v3, v4):
-        return self.g.are_connected(v2, v3) or self.g.are_connected(v4, v1)
+        return self.g.are_connected(v2, v4) and self.g.are_connected(v1, v2) or self.g.are_connected(v1, v4) and self.g.are_connected(v3, v4)
 
     def vertex_exists(self, v):
         return v in self.g.vs
@@ -94,7 +96,7 @@ class MyGraph:
         return self.g.are_connected(v1, v2)
 
     def is_graph_empty(self):
-        return self.g.is_empty()
+        return self.g.ecount() == 0 and self.g.vcount() > 0
 
     def is_full_graph(self):
         for i in range(self.g.vcount()):
@@ -117,10 +119,10 @@ class MyGraph:
     def is_bridge_naive(self, v1, v2):
         bridges = find_bridges_naive(self.g)
         return (v1, v2) in bridges
-    
+
     def clone(self):
         return MyGraph(g=self.g.copy())
-    
+
     def fleury(self, method="TARJAN"):
         # Make sure the graph has either 0 or 2 odd vertices
         if not self.is_connected():
@@ -145,34 +147,40 @@ class MyGraph:
                     start = i
                     break
         path = graph.fleury_rec(graph, start, [start], method="TARJAN")
-        outp = "Euleriano" if path[0] == path[len(path) - 1] else "Semi-Euleriano"
+        outp = "Euleriano" if path[0] == path[len(
+            path) - 1] else "Semi-Euleriano"
         return [outp, path]
-        
+
     def fleury_rec(self, graph, v, path, method):
         if graph.g.ecount() == 0:
             return path
         for e in graph.g.get_adjlist()[v]:
             if not (graph.is_bridge_tarjan(v, e) if method == "TARJAN" else graph.is_bridge_naive(v, e)):
                 break
-        graph.g.delete_edges([(v,e)])
+        graph.g.delete_edges([(v, e)])
         path.append(e)
         return graph.fleury_rec(graph, e, path, method)
-        
-    def export_graph(self):
-        self.g.write('graph.dot') 
 
-    def import_graph(self, file, format):
-        self.g = Graph.Read(file, format=format)
-        pass
+    def export_graph(self, filename="output.gml", format="gml"):
+        ## Export graph to file on graphml format
+        self.g.write(filename, format)
+
+    def import_graph(self, filename="output.gml", format="gml"):
+        ## Import graph from file on graphml format
+        return MyGraph(g= Graph.Load(filename, format=format))
 
     def show(self):
         visual_style = {}
         visual_style["vertex_size"] = 0.5
         visual_style["vertex_color"] = "white"
         visual_style["edge_width"] = 0.5
+        visual_style["edge_color"] = "black"
+        visual_style["edge_size"] = 0.1
+        visual_style["edge_align_label"] = True
+
         tx, ax = plt.subplots()
-        ig.plot(self.g, target=ax, **visual_style)
+        ig.plot(self.g, layout="kk", target=ax, **visual_style)
         plt.show()
-        
+
     def is_connected(self):
         return self.g.is_connected()
